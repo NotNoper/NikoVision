5636714a513844758115e84a69dee3f07117161b
NotNoper <n.attilako@gmail.com>
2025-05-23 13:29:56 +0300

    undid

--- server.py
+++ server.py
@@ -56,8 +56,7 @@ def upload_image():
         params = {
             "engine": "google_reverse_image",
             "image_url": url_param,
-            "api_key": SERPAPI_API_KEY,
-            "tbs": "itp:photo,isz:l"
+            "api_key": SERPAPI_API_KEY
         }
 
         search = requests.get("https://serpapi.com/search", params=params)
